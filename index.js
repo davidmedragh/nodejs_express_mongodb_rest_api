a232353af1167f74e50974197b8093d868ad9eba
david MEDRAGH <david.medragh@gmail.com>
2020-03-19 22:26:12 +0100

    add mangoose and mongodb crud rest API

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended : true}));
 app.use(bodyParser.json());
 
 routes(app);
+app.use(express.static('public'));
 
 app.get('/',(req,res) =>
     res.send(`Serveur node et express sur port ${PORT}`)
